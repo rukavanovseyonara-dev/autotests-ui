0b2be373042a695c9c72df02faa6090b3c32d94b
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2025-09-22 19:18:48 +0500

    poluchaetsya kaifuem

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
@@ -8,7 +8,7 @@ with sync_playwright() as playwright:
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+    email_input.fill('user.name1@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
     username_input.fill('username')
@@ -19,13 +19,27 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path="kaifuem.json")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_drawer = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_drawer).to_be_visible()
+
+    view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(view_title).to_be_visible()
+
+    view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(view_icon).to_be_visible()
+
+    view_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(view_text).to_be_visible()
+
+
 
     page.wait_for_timeout(5000)
