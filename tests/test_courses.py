3bd91d564217d8cea191ed1d0dcc81917f96090f
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2025-10-02 18:12:57 +0500

    Интеграция Pytest и Playwright. Практика написания автотестов

---
+++ tests/test_courses.py
@@ -0,0 +1,46 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_empty_courses_list():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name1@gmail.com')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        context.storage_state(path="kaifuem.json")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="kaifuem.json")
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+        courses_drawer = page.get_by_test_id('courses-list-toolbar-title-text')
+        expect(courses_drawer).to_be_visible()
+        expect(courses_drawer).to_have_text('Courses')
+
+        view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+        expect(view_title).to_be_visible()
+        expect(view_title).to_have_text('There is no results')
+
+        view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        expect(view_icon).to_be_visible()
+
+        view_text = page.get_by_test_id('courses-list-empty-view-description-text')
+        expect(view_text).to_be_visible()
+        expect(view_text).to_have_text('Results from the load test pipeline will be displayed here')
