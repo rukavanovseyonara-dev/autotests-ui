98e7ef34f3fa7611b8884445375726104b5a09bd
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2025-10-21 20:18:50 +0500

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,36 +1,11 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name1@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path="kaifuem.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="kaifuem.json")
-        page = context.new_page()
-
+def test_empty_courses_list(chromium_page_with_state):
+        page = chromium_page_with_state
         page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
         courses_drawer = page.get_by_test_id('courses-list-toolbar-title-text')
