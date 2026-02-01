706a269a48f0142e31a2e8ae971a08a521276da7
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2026-02-01 11:35:40 +0500

    Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,6 +6,17 @@ from pages.courses_list_page import CoursesListPage
 
 @pytest.mark.courses
 @pytest.mark.regression
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
+
+
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
