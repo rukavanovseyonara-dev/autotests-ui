cf9a04e92c676301fa8cf8a052a9bbe2d4b8910c
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2026-03-31 19:43:45 +0500

    Мелкие правки

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,21 +5,26 @@ from _pytest.fixtures import SubRequest
 
 from tools.playwright.pages import initialize_playwright_page
 from config import settings
+from tools.routes import AppRoute
 
 
-@pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright):
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+@pytest.fixture(params=settings.browsers)
+def page(request: SubRequest, playwright: Playwright):
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param
+    )
 
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.visit(AppRoute.REGISTRATION)
     registration_page.registration_form_component.fill(
         email=settings.test_user.email,
         username=settings.test_user.username,
@@ -31,10 +36,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright):
+@pytest.fixture(params=settings.browsers)
+def page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright):
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
+        browser_type=request.param,
         storage_state=settings.browser_state_file
     )
